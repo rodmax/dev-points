3388b8541928cbcaf752acafcb249ce809eea2ef
max.rodionov <rodionov.m.m@gmail.com>
2021-12-25 13:21:53 +0300

    refactor: clean config a bit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,9 +15,9 @@ module.exports = {
     onBrokenMarkdownLinks: 'warn',
     themeConfig: {
         navbar: {
-            title: 'My Site',
+            title: 'Dev tips store by Max Rodionov',
             logo: {
-                alt: 'My Site Logo',
+                alt: 'Dev Tips Logo',
                 src: 'img/logo.svg',
             },
             items: [
@@ -29,7 +29,7 @@ module.exports = {
                 },
                 { to: '/blog', label: 'Blog', position: 'left' },
                 {
-                    href: 'https://github.com/facebook/docusaurus',
+                    href: 'https://github.com/rodmax',
                     label: 'GitHub',
                     position: 'right',
                 },
@@ -47,23 +47,23 @@ module.exports = {
                         },
                     ],
                 },
-                {
-                    title: 'Community',
-                    items: [
-                        {
-                            label: 'Stack Overflow',
-                            href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-                        },
-                        {
-                            label: 'Discord',
-                            href: 'https://discordapp.com/invite/docusaurus',
-                        },
-                        {
-                            label: 'Twitter',
-                            href: 'https://twitter.com/docusaurus',
-                        },
-                    ],
-                },
+                // {
+                //     title: 'Community',
+                //     items: [
+                //         {
+                //             label: 'Stack Overflow',
+                //             href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+                //         },
+                //         {
+                //             label: 'Discord',
+                //             href: 'https://discordapp.com/invite/docusaurus',
+                //         },
+                //         {
+                //             label: 'Twitter',
+                //             href: 'https://twitter.com/docusaurus',
+                //         },
+                //     ],
+                // },
                 {
                     title: 'More',
                     items: [
@@ -72,13 +72,13 @@ module.exports = {
                             to: '/blog',
                         },
                         {
-                            label: 'GitHub',
-                            href: 'https://github.com/facebook/docusaurus',
+                            label: 'My GitHub',
+                            href: 'https://github.com/rodmax',
                         },
                     ],
                 },
             ],
-            copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+            copyright: `Copyright © ${new Date().getFullYear()} Max Rodionov, Inc. Built with Docusaurus.`,
         },
         prism: {
             theme: lightCodeTheme,
